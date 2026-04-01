8f8c86af6ea683fa8f4516d27506acad7267e8e5
PeatroXD <PeatroXD@yandex.ru>
2026-04-01 12:38:10 +0300

    Init

---
+++ src/main/java/com/peatroxd/mtprototest/MtprototestApplication.java
@@ -0,0 +1,13 @@
+package com.peatroxd.mtprototest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtprototestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtprototestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peatroxd/mtprototest/MtprototestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peatroxd.mtprototest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtprototestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
