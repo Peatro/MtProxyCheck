527c38f4befe55ff15e7f57ed5b32fafff37aa99
PeatroXD <PeatroXD@yandex.ru>
2026-04-02 13:41:22 +0300

    feat: feedback system

--- src/main/java/com/peatroxd/mtprototest/proxy/controller/ProxyController.java
+++ src/main/java/com/peatroxd/mtprototest/proxy/controller/ProxyController.java
@@ -1,8 +1,14 @@
 package com.peatroxd.mtprototest.proxy.controller;
 
+import com.peatroxd.mtprototest.proxy.dto.request.ProxyFeedbackRequest;
+import com.peatroxd.mtprototest.proxy.dto.response.ProxyFeedbackResponse;
 import com.peatroxd.mtprototest.proxy.dto.response.ProxyResponse;
+import com.peatroxd.mtprototest.proxy.service.ProxyFeedbackService;
 import com.peatroxd.mtprototest.proxy.service.ProxyService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +21,18 @@ import java.util.List;
 public class ProxyController {
 
     private final ProxyService proxyService;
+    private final ProxyFeedbackService proxyFeedbackService;
 
     @GetMapping("/best")
     public List<ProxyResponse> best() {
         return proxyService.getBest();
+    }
+
+    @PostMapping("/{proxyId}/feedback")
+    public ProxyFeedbackResponse submitFeedback(
+            @PathVariable Long proxyId,
+            @RequestBody ProxyFeedbackRequest request
+    ) {
+        return proxyFeedbackService.submitFeedback(proxyId, request);
     }
 }
