435629e0707219da807372353b2e4bbd9548de3f
Peatro <PeatroXD@yandex.ru>
2026-07-02 02:41:16 +0300

    Change configuration prefix from 'app.sources' to 'proxy.sources'

--- src/main/java/com/peatroxd/mtprototest/parser/config/ParserSourcesProperties.java
+++ src/main/java/com/peatroxd/mtprototest/parser/config/ParserSourcesProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.ArrayList;
 import java.util.List;
 
-@ConfigurationProperties(prefix = "app.sources")
+@ConfigurationProperties(prefix = "proxy.sources")
 public class ParserSourcesProperties {
 
     private List<SourceDefinition> entries = new ArrayList<>();
